08e67885623875b29ad90de17e4b208c1af242dd
Nigel Tufnel <tenorideout@gmail.com>
2018-06-20 09:47:20 -0400

    Definitely not even close to being done

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+$( document ).ready(function) {
+
+
+
+    var random=Math.floor(math.random() * 101 + 19;
+
+    $("#randomNumber").text(rand);
+    console.log("#randomNumber")
+
+    var num1= Math.floor(Math.random() *11+1)
+    var num2= Math.floor(Math.random() *11+1)
+    var num3= Math.fllor(Math.random() *11+1)
+    var num4= Math.fllor(Math.random() *11+1)
+    console.log(num2);
+
+    var tscore = 0
+    var wins = 0
+    var losses = 0
+
+    $('#winsNum').text(wins);
+    $('#lossNum').text(losses);
+
+    function reset() {
+        random=Math.floor(math.random() * 101 +19;
+        $("#randomNumber").text(rand);
+        console.log(randomNumber);
+
+        var num1= Math.floor(Math.random() *11+1)
+        console.log(num1);
+        var num2= Math.floor(Math.random() *11+1)
+        var num3= Math.fllor(Math.random() *11+1)
+        var num4= Math.fllor(Math.random() *11+1)
+        var wins = 0
+        var losses = 0
+    
+    
+    )
+    }
+}
